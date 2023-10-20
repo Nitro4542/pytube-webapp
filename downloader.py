5bd5245262288d9f3de3c9f7c03c755cb6e979c6
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2023-10-20 18:44:14 +0200

    Improved code quality

--- downloader.py
+++ downloader.py
@@ -1,3 +1,4 @@
+# Import dependencies
 from pytube import YouTube
 
 
@@ -8,15 +9,18 @@ class Downloader:
         self.stream = None
 
     def download_video(self):
+        """Downloads the video of a YouTube video"""
         video = YouTube(self.url)
         self.stream = video.streams.get_highest_resolution()
         self.stream.download(self.dl_path)
 
     def download_audio(self):
+        """Downloads the audio stream of a YouTube video"""
         video = YouTube(self.url)
         self.stream = video.streams.get_by_itag(251)
         self.stream.download(self.dl_path)
 
     def full_path(self, file_format):
-        full_path = self.dl_path + self.stream.title + '.' + file_format
+        """Returns the full path where the video is stored"""
+        full_path = self.dl_path + "/" + self.stream.title + '.' + file_format
         return full_path
