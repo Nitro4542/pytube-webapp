d913ebebe593bed5244a026104a3c46b16de9d93
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2023-12-25 13:06:38 +0100

    Improve Colab Support

---
+++ main-colab.py
@@ -0,0 +1,40 @@
+"""A web-based front end for pytube on Google Colab"""
+# Import dependencies
+from flask import Flask, render_template, request, send_file
+from flask_ngrok import run_with_ngrok
+from downloader import Downloader
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def start_page():
+    """Main page"""
+    if request.method == 'POST':
+        # Get form data from HTMl file
+        url = request.form.get('video_url')
+        dl_format = request.form.get('btnradio')
+        # Set download path
+        dl_path = 'download'
+
+        # Check how the video will be downloaded
+        if dl_format == "video":
+            # Create a Downloader object and set download folder
+            dl = Downloader(url, dl_path)
+            dl.download_video()
+            # Send downloaded video to the client
+            return send_file(dl.full_path('mp4'), as_attachment=True)
+        elif dl_format == "audio":
+            # Create a Downloader object and set download folder
+            dl = Downloader(url, dl_path)
+            dl.download_audio()
+            # Send downloaded video to the client
+            return send_file(dl.full_path('webm'), as_attachment=True)
+
+    # Display the main page
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
